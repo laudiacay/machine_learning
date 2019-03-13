65b147791691151732a2954ea78f2c581b2913a7
Claude Richoux <laudecay@gmail.com>
2019-03-13 10:31:13 +0000

    fd

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import read_data as rd
+import random
 
 LEARNING_RATE = 1e-3
 
@@ -58,37 +59,73 @@ def train(sentences, enc_model, dec_model, enc_opt, dec_opt, criterion):
     _, (hidden, ctx) = enc_model.forward(sent_1, None, None)
     output, _ = dec_model.forward(sent_2, hidden.detach(), ctx.detach())
 
-    preds = [rd.get_word_from_embedded(out) for out in output]
-    print(preds)
+    #preds = [rd.get_word_from_embedded(out) for out in output]
+    #print(preds)
 
     enc_opt.zero_grad()
     dec_opt.zero_grad()
     loss = 0
     for out, exp_out in zip(output, sent_2):
         loss += criterion(out, exp_out)
+        total_accuracy += int(rd.get_word_from_embedded(out)\
+                            == rd.get_word_from_embedded(exp_out))
+    accuracy = total_accuracy / min(len(output), len(sent_2))
     loss.backward()
     enc_opt.step()
     dec_opt.step()
-    return loss
+    return output, loss, accuracy
 
 def predict(sent_1, enc_model, dec_model):
     _, (hidden, ctx) = enc_model(sent_1, None, None)
 
+def train_main():
+    train_data, dev_data, test_data = rd.get_embedded_data()
 
-train_data, dev_data, test_data = rd.get_embedded_data()
+    enc_model = LSTM(200, 200)
+    dec_model = LSTM(200, 200)
 
-enc_model = LSTM(200, 200)
-dec_model = LSTM(200, 200)
+    # PERFORMANCE: torch.optim.Adam?
+    enc_opt = torch.optim.SGD(enc_model.parameters(), lr=LEARNING_RATE)
+    dec_opt = torch.optim.SGD(dec_model.parameters(), lr=LEARNING_RATE)
 
-# PERFORMANCE: torch.optim.Adam?
-enc_opt = torch.optim.SGD(enc_model.parameters(), lr=LEARNING_RATE)
-dec_opt = torch.optim.SGD(dec_model.parameters(), lr=LEARNING_RATE)
+    # PERFORMANCE: change loss to softmax/cross-entropy loss?
+    loss_func = nn.MSELoss()
 
-# PERFORMANCE: change loss to softmax/cross-entropy loss?
-loss_func = nn.MSELoss()
+    N_ITERS = 100000
+    logint = 10000
+    losses = []
+    all_losses = []
+    accuracies = []
+    start = time.time()
 
-N_ITERS = 100000
-logint = 10000
-losses = []
-all_losses = []
-train(train_data[0], enc_model, dec_model, enc_opt, dec_opt, loss_func)
+    for i in range(N_ITERS):
+        training_example = random.choice(train_data)
+        out, loss, accuracy = train(training_example, enc_model, dec_model, enc_opt, dec_opt, loss_func)
+        losses.append(loss)
+        all_losses.append(loss)
+        accuracies.append(accuracy)
+        if i % logint == 0:
+            elapsed = (time.time() - start) / 60
+            avg_loss = np.mean(losses)
+            acc = np.mean(accuracies)
+            print('Iter {:7} | Loss: {:5.3f} | Acc: {:.3f} | Elapsed: {:.2f}min'.format(i, avg_loss, acc, elapsed))
+            accuracies = []
+            losses = []
+
+    torch.save(enc_model.state_dict(), 'enc_model.pth')
+    torch.save(dec_model.state_dict(), 'dec_model.pth')
+
+def load_main():
+    print('loading models from files!')
+    enc_model = LSTM(200, 200)
+    enc_model.load_state_dict(torch.load('enc_model.pth'))
+    enc_model.eval()
+    dec_model = LSTM(200, 200)
+    dec_model.load_state_dict(torch.load('dec_model.pth'))
+    dec_model.eval()
+
+if __name__ == '__main__':
+    if TRAIN:
+        train_main()
+    else:
+        load_main()
