56b60b6eb47c15143f685fb5b6c3cacb019b73fc
Claude Richoux <laudecay@gmail.com>
2019-03-13 08:58:10 +0000

    fdsa

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -1,32 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import itertools
+import read_data
 
 LEARNING_RATE = 1e-3
-EMBEDDING_SIZE = 200
 
 torch.manual_seed(42)
 
-def read_vocab(vocab_file):
-    # puts words from vocab_file into a list of strings
-    with open(vocab_file) as f:
-        vocab_list = f.read().splitlines()
-    return vocab_list
-
-# returns a list of sentences
-# where a sentence is represented as a list of word strings
-def read_tsv(tsv_file):
-    with open(tsv_file) as f:
-        examples = f.read().splitlines()
-    return [e.split() for e in examples]
-
-def example_to_tensor(lines):
-    max_line_len = max([len(l) for l in lines])
-    tensor = torch.zeros(max_line_len, len(lines), EMBEDDING_SIZE)
-
-
-class LSTM(nn.Module): 
+class LSTM(nn.Module):
     def __init__(self, inp_size, hid_size):
         super(LSTM, self).__init__()
         self.inp_size = inp_size
@@ -35,8 +16,12 @@ class LSTM(nn.Module):
         self.Wf = nn.Linear(self.inp_size + self.hid_size, self.hid_size)
         self.Wo = nn.Linear(self.inp_size + self.hid_size, self.hid_size)
         self.Wg = nn.Linear(self.inp_size + self.hid_size, self.hid_size)
+        # PERFORMANCE: add log softmax layer?
 
     def forward(self, x_seq, h_0, c_0):
+        if h_0 is None: h_0 = self.init_hidden()
+        if c_0 is None: c_0 = self.init_hidden()
+
         def one_step(x, h_i, c_i):
             stacked = torch.cat((x, h_i), 0)
             i = F.sigmoid(self.Wi(stacked))
@@ -46,34 +31,29 @@ class LSTM(nn.Module):
             c_i1 = f * c_i + i * g
             h_i1 = o * F.tanh(c_i1)
             return h_i1, c_i1
-        
+
         out = []
         h_t, c_t = h_0, c_0
-        
+
         for x in x_seq.size(0):
             h_t, c_t = one_step(x, h_t, c_t)
             out.append(h_t)
-        
+
         out = torch.cat(out, 0)
 
         return out, h_t, c_t
 
-# PERFORMANCE: set scale_grad_by_freq = True in embedding?
-# PERFORMANCE: change distribution of embedding to uniform on [-0.1, 0.1]?
-# PERFORMANCE: set max_norm in embedding?
-# PERFORMANCE: should I treat </s> as another embedding?
-vocab_lst = read_vocab('bobsue-data/bobsue.voc.txt')
-vocab_ixs = {k: v for v, k in enumerate(vocab_lst)}
-embedding = nn.Embedding(len(vocab_lst), 200)
+    def init_hidden(self):
+        return torch.zeros(1, self.hid_size)
 
-train_raw = read_tsv('bobsue-data/bobsue.seq2seq.train.tsv')
-dev_raw = read_tsv('bobsue-data/bobsue.seq2seq.dev.tsv')
-test_raw = read_tsv('bobsue-data/bobsue.seq2seq.test.tsv')
+def train(, model, optimizer, criterion):
 
-print(train_raw)
+encoder_model = LSTM(200, 200)
+decoder_model = LSTM(200, 200)
 
-model = LSTM(200, 200)
+# PERFORMANCE: torch.optim.Adam?
 opt = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
 # PERFORMANCE: change loss to softmax/cross-entropy loss?
 loss_func = nn.MSELoss()
 
---
+++ lstm/read_data.py
@@ -0,0 +1,52 @@
+import torch
+import random
+
+EMBEDDING_SIZE = 200
+
+# PERFORMANCE: set scale_grad_by_freq = True in embedding?
+# PERFORMANCE: change distribution of embedding to uniform on [-0.1, 0.1]?
+# PERFORMANCE: set max_norm in embedding?
+# PERFORMANCE: should I treat </s> as another dimension?
+vocab_lst = read_vocab('bobsue-data/bobsue.voc.txt')
+vocab_ixs = {k: v for v, k in enumerate(vocab_lst)}
+embedding = (torch.rand([len(vocab_lst), EMBEDDING_SIZE]) / 5.0) - 0.1
+
+def read_vocab(vocab_file):
+    # puts words from vocab_file into a list of strings
+    with open(vocab_file) as f:
+        vocab_list = f.read().splitlines()
+    return vocab_list
+
+# returns a list of sentences
+# where a sentence is represented as a list of word strings
+def read_tsv(tsv_file):
+    with open(tsv_file) as f:
+        examples = f.read().splitlines()
+    return [e.split() for e in examples]
+
+# list of word strings -> list of tensors of embeddings for words
+def sentence_list_to_tensors(sentence):
+    tensors = []
+    for word in sentence:
+        tensors.append(embedding[vocab_ixs[word]])
+    return tensors
+
+def all_data_to_tensors(sentences):
+    return [sentence_list_to_tensors(sentence) for sentence in sentences]
+
+def get_embedded_data():
+    train_raw = read_tsv('bobsue-data/bobsue.seq2seq.train.tsv')
+    dev_raw = read_tsv('bobsue-data/bobsue.seq2seq.dev.tsv')
+    test_raw = read_tsv('bobsue-data/bobsue.seq2seq.test.tsv')
+    train_data = all_data_to_tensors(train_raw)
+    dev_data = all_data_to_tensors(dev_raw)
+    test_data = all_data_to_tensors(test_raw)
+    return train_data, dev_data, test_data
+
+def get_word_from_embedded(output):
+    dotted = torch.matmul(embedding, output)
+    softmaxed = torch.nn.functional.softmax(dotted)
+    return vocab_lst[softmaxed.max()]
+
+print(vocab_lst[9230])
+print(get_word_from_embedded(embedding[9230]))
