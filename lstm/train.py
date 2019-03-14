2dbc0e8321aecdfd1aa2f07b89ebf0497888f9df
Claude Richoux <laudiacay@cs.uchicago.edu>
2019-03-14 17:28:13 -0500

    fuck

--- lstm/train.py
+++ lstm/train.py
@@ -1,31 +1,37 @@
 import torch
-cuda = torch.device('cuda')
-torch.cuda.manual_seed(42)
+#torch.manual_seed(442)
 
 from read_data import load, PAD_TOK
 from lstm import Seq2Seq
+import time
+from tqdm import tqdm
 
-HIDDEN_SIZE = 200
-LEARNING_RATE = 0.05  # maybe try 0.5?
-N_EPOCHS = 15
+HIDDEN_SIZE = 64
+LEARNING_RATE = 0.5
+N_EPOCHS = 25
 
-# TODO: definitely some dimension issues here...
-def calc_accuracy(output, target):
-    preds = [o.max(1)[1] for o in output]
-    correct_list = [o == t for o, t in zip(output[1:], target[1:])]
-    return sum(correct_list)/max(len(output) - 1, len(target) - 1)
+def calc_accuracy(output, target, pad_ind=0):
+    _, preds = output.max(1)
+    correct_count = 0
+    total_count = 0
+    for i in range(preds.shape[0]):
+        for j in range(preds.shape[1]):
+            if target[i, j] != pad_ind:
+                total_count += 1
+                if target[i, j] == preds[i, j]: correct_count += 1
+    return correct_count / total_count
 
-# TODO: do I need to add clipping to prevent exploding grads?
-def train(data_iterable, model, opt, loss_func):
+def train(data_iterable, model, opt, loss_func, pad_ind=0):
     # turn on dropouts, if you use them
     # model.train()
     epoch_losses = []
     epoch_accuracies = []
+    num_batches = len(data_iterable)
     for batch in tqdm(data_iterable):
         opt.zero_grad()
         output = model.forward(batch.sent_1, batch.sent_2)
-        loss = loss_func(output, batch.sent_2)
-        accuracy = calc_accuracy(output, batch.sent_2)
+        loss = loss_func(output, batch.sent_2[1:])
+        accuracy = calc_accuracy(output, batch.sent_2[1:], pad_ind=pad_ind)
         loss.backward()
         # TODO: clipping?
         opt.step()
@@ -35,7 +41,13 @@ def train(data_iterable, model, opt, loss_func):
     avg_accuracy = sum(epoch_accuracies)/len(epoch_accuracies)
     return avg_loss, avg_accuracy
 
-def eval_model(data_iterable, model, loss_func):
+def pad_out(output, target, pad_ind):
+    output = output[:min(output.shape[0], target.shape[0]), :, :]
+    new_out = torch.full([target.shape[0], output.shape[1], output.shape[2]], pad_ind)
+    new_out[:output.shape[0], :, :] = output
+    return new_out
+
+def eval_model(data_iterable, model, loss_func, pad_ind=0):
     # turn off dropouts, if you use them
     # model.eval()
     epoch_losses = []
@@ -43,35 +55,50 @@ def eval_model(data_iterable, model, loss_func):
     # speed things up by not calculating gradients, we aren't backpropping
     with torch.no_grad():
         for batch in tqdm(data_iterable):
-            output = model.forward(batch.sent_1, None)
-            loss = loss_func(output, batch.sent_2)
-            accuracy = calc_accuracy(output, batch.sent_2)
+            output = model.forward(batch.sent_1, None) 
+            output = pad_out(output, batch.sent_2[1:], pad_ind)
+            loss = loss_func(output, batch.sent_2[1:])
+            accuracy = calc_accuracy(output, batch.sent_2[1:], pad_ind=pad_ind)
             epoch_losses.append(loss)
             epoch_accuracies.append(accuracy)
     avg_loss = sum(epoch_losses)/len(epoch_losses)
     avg_accuracy = sum(epoch_accuracies)/len(epoch_accuracies)
     return avg_loss, avg_accuracy
 
+def indices_to_words(phrase, SENTENCES):
+    return [SENTENCES.vocab.itos[word] for word in phrase]
+
+
+def eval_once(data_iterable, model, SENTENCES):
+    with torch.no_grad():
+        for batch in tqdm(data_iterable):
+            output = model.forward(batch.sent_1, None)
+            _, preds = output.max(1)
+            print('sent_1', indices_to_words(batch.sent_1, SENTENCES))
+            print('sent_2', indices_to_words(batch.sent_2, SENTENCES))
+            print('preds!', indices_to_words(preds, SENTENCES))
+
 def train_main():
-    train_iter, dev_iter, test_iter, SENTENCES = load(cuda)
+    train_iter, dev_iter, test_iter, SENTENCES = load()
     torch.save(SENTENCES.vocab.vectors, 'vectors.pth')
 
-    model = Seq2Seq(HIDDEN_SIZE, SENTENCES).cuda()
+    PAD_IND = SENTENCES.vocab.stoi[PAD_TOK]
 
-    # PERFORMANCE: Adam for optimization?
-    # opt = torch.optim.Adam(dec_model.parameters(), lr=LEARNING_RATE)
-    opt = torch.optim.SGD(dec_model.parameters(), lr=LEARNING_RATE)
+    model = Seq2Seq(HIDDEN_SIZE, SENTENCES)
+
+    opt = torch.optim.Adadelta(model.parameters(), lr=LEARNING_RATE)
 
     # PERFORMANCE: change loss to softmax/cross-entropy loss?
-    # loss = nn.CrossEntropyLoss(ignore_index=SENTENCES.vocab.stoi[PAD_TOK])
-    loss = nn.MSELoss(ignore_index=SENTENCES.vocab.stoi[PAD_TOK])
+    loss = torch.nn.CrossEntropyLoss(ignore_index=PAD_IND)
+    # loss = torch.nn.MSELoss(ignore_index=SENTENCES.vocab.stoi[PAD_TOK])
 
     min_dev_loss = float('inf')
 
     start = time.time()
     for epoch in range(N_EPOCHS):
-        training_loss, training_acc = train(train_iter, model, opt, loss)
-        dev_loss, dev_acc = eval_model(dev_iter, model, loss)
+        training_loss, training_acc = train(train_iter, model, opt, loss,
+                pad_ind=PAD_IND)
+        dev_loss, dev_acc = eval_model(dev_iter, model, loss, pad_ind=PAD_IND)
         end = time.time()
 
         if dev_loss < min_dev_loss:
@@ -79,28 +106,29 @@ def train_main():
             min_dev_acc = dev_acc
             torch.save(model.state_dict(), 'model.pth')
 
-        print(f'Epoch {epoch+1} | Elapsed: {(end - start) / 60:3.3f}m')
-        print(f'    Train Loss: {training_loss:.3f}')
-        print(f'    Train Acc:  {training_acc:.3f}')
-        print(f'    Dev Loss:   {dev_loss:.3f}')
-        print(f'    Dev Acc:    {dev_acc:.3f}')
+        print('Epoch {} | Elapsed: {:3.3}m'.format(epoch+1, (end - start) / 60))
+        print('    Train Loss: {:.3}'.format(training_loss))
+        print('    Train Acc:  {:.3}'.format(training_acc))
+        print('    Dev Loss:   {:.3}'.format(dev_loss))
+        print('    Dev Acc:    {:.3}'.format(dev_acc))
         print()
-    print(f'Total Elapsed Time: {(end - start) / 60:3.3f}m')
-    print(f'Final Saved Dev Loss: {min_dev_acc:.3f}')
-    print(f'Final Saved Dev Acc:  {min_dev_acc:.3f}')
+    print('Total Elapsed Time: {:3.3}m'.format((end - start) / 60))
+    print('Final Saved Dev Loss: {:.3}'.format(min_dev_loss))
+    print('Final Saved Dev Acc:  {:.3}'.format(min_dev_acc))
 
 def load_main():
     print('loading data and vector embeddings')
-    vectors = torch.load('vectors.pth').to(cuda)
-    train_iter, dev_iter, test_iter, SENTENCES = load()
+    vectors = torch.load('vectors.pth')
+    train_iter, dev_iter, test_iter, SENTENCES = load(train_batch_size=1)
     SENTENCES.vocab.vectors = vectors
     print('loaded data and vector embeddings')
     print('loading model from files')
-    model = Seq2Seq(HIDDEN_SIZE, SENTENCES).cuda()
+    model = Seq2Seq(HIDDEN_SIZE, SENTENCES)
     model.load_state_dict(torch.load('model.pth'))
     model.eval()
     print('loaded model')
-
+    #eval_once(dev_iter, model, SENTENCES) 
+    eval_once(train_iter, model, SENTENCES)
     # TODO: do predictions, print them, get accuracies
 
 if __name__ == '__main__':
